c102159d529db025544bead0c78c33cf776167e6
Derek Zhou <dzhou84@gatech.edu>
2021-09-29 14:24:11 -0400

    icon update

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ProfileScreen from './app/screens/ProfileScreen';
+import SearchScreen from './app/screens/SearchScreen';
 
 export default function App() {
   return <ProfileScreen />;
