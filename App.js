6291e3242413918b4266e1fe8b44a0d650099c12
Derek Zhou <dzhou84@gatech.edu>
2021-11-08 17:54:46 -0500

    tab navigator

--- App.js
+++ App.js
@@ -4,9 +4,43 @@ import { StyleSheet, Text, View } from 'react-native';
 import ProfileScreen from './app/screens/ProfileScreen';
 import SearchScreen from './app/screens/SearchScreen';
 import RemindersScreen from './app/screens/RemindersScreen';
+import Colors from './app/screens/Colors';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { COLORS } from './app/screens/Colors';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return <RemindersScreen />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+      screenOptions = {({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.purple,
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          if (route.name == 'Today') {
+            iconName = focused ? 'calendar' : 'calendar-outline';
+          } else if (route.name == 'Reminders') {
+            iconName = focused ? 'notifications' : 'notifications-outline';
+          } else if (route.name == 'Search') {
+            iconName = focused ? 'search' : 'search-outline';
+          } else if (route.name == 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+          return <Ionicons name = {iconName} size = {size} color = {color}/>;
+        },
+      })}>
+        <Tab.Screen name = "Today" component = {RemindersScreen} />
+        <Tab.Screen name = "Reminders" component={RemindersScreen} />
+        <Tab.Screen name = "Search" component = {SearchScreen} />
+        <Tab.Screen name = "Profile" component = {ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
